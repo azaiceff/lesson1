65a4071b19f3a4fe53271ffbd0b39ecda7589332
Test <zaiseffz@gmail.com>
2022-04-10 14:09:10 +0300

    Homework 4 finel correct

--- src/Lesson4/Lesson4.java
+++ src/Lesson4/Lesson4.java
@@ -24,8 +24,8 @@ public class Lesson4 {
     }
 
     private static void start(){
-        while (true) {//Здесь немного по другому сделал чем в методичке. Даю возможность ИИ в последний момент
-            humanTurn();                //подменить крестик на нолик, благодаря чему игра может продолжиться.
+        while (true) {
+            humanTurn();
             map();
             aiTurn();
             map();
@@ -56,20 +56,21 @@ public class Lesson4 {
 
     private static void map(){
         System.out.print("YX");
+        int sizeX = 9;
         for (int i = 0; i < size; i++){
-            if (i > 9){
+            if (i > sizeX){
                 System.out.print((i + 1) + " ");
             }else
             System.out.print(" " + (i + 1) + " ");
         }
         System.out.println();
+        int sizeY = 8;
         for (int i = 0; i < size; i++){
-            if (i > 8) {
+            if (i > sizeY) {
                 System.out.print(i + 1);
             }else
             System.out.print(i + 1 + " ");
             System.out.println(Arrays.toString(map[i]));
-
         }
     }
 
@@ -88,48 +89,28 @@ public class Lesson4 {
             System.out.println("Введите координаты в формате X Y");
             x = sc.nextInt() - 1;
             y = sc.nextInt() - 1;
-        } while (isCellValid(x, y));
+        } while (isCellValid(x, y, dot_x));
         map[y][x] = dot_x;
-
     }
 
     private static void aiTurn() {
-        /*Это то, что касается задания о помощи ИИ в победе.
-         С таким режимом ентому негодяю и помогать уже не хочется, обманщик епт, а не ИИ.
-         Такой простой метод, а так прикольно с ним играть стало (с другой стороны, если понимаешь как это работает,
-         обыграть все равно очень просто).
-         Мысль, конечно, как ему помочь (так скажем законным путем) у меня есть. Благодаря тому,
-         что я вынес проверки длинны линий в отдельные методы, можно попробовать их так же использовать для определения
-         положения формирующийся линии и на основании этого выбрать координаты ответного хода ИИ*/
         int x,y;
-        if (checkWin(dot_x, line - 2) && size > 4){//можно отключить этот режим на маленьком поле
-            do {
-                x = random.nextInt(map.length);
-                y = random.nextInt(map[x].length);
-            }while(/*!isCellValid(x, y) */ map[y][x] != dot_x);
-            //Можно поменять условие, будет по легче. Но в любом случае, есл понимаешь то обыграть ИИ легко
-            map[y][x] = dot_o;
-        }else
         do {
             x = random.nextInt(map.length);
             y = random.nextInt(map[x].length);
-        }while(isCellValid(x, y));
+        }while(isCellValid(x, y, dot_o));
         map[y][x] = dot_o;
         System.out.println("\nИИ сделал ход по координатам: " + "\nx = " + (x + 1) + " y = " + (y + 1));
     }
 
-    private static boolean isCellValid (int x, int y){
-        return (x < 0 || x >= size || y < 0 || y >= size) || map[y][x] != dot_empty;
+    private static boolean isCellValid (int x, int y, char symb){
+        if (checkWin(dot_x, line - 2) && size > 4 && symb == dot_o) {
+            return map[y][x] != dot_x;
+        } else {
+            return (x < 0 || x >= size || y < 0 || y >= size) || map[y][x] != dot_empty;
+        }
     }
 
-    /*Здесь начинаются методы проверки полученной длинны линий
- Пробовал сделать как-то по другому, но... То условия начинают конфликтовать между собой, то явно
-  начинает попахивать говно-кодом и тд. Решил, что для меня пока оптимальнее оставить так
-  Правильнее конечно пойти было другим путем. Сосредоточиться на методах, которым бы я передавал
-  вводимые координаты и относительно них проверял бы длину линий. Но с точки зрения тестирования,
-   выбранный мной путь, наверное правильнее (с каждым ходом, я как бы заново проверяю всю карту на случай,
-   не произошли ли в ней какие сбои. Причем несколько раз немного разными алгоритмами, что повышает покрытие
-   и качество тестирования)*/
     private static int horizontalLine (char symb){
         int maxStr = 0;
         for (char[] chars : map) {
@@ -146,7 +127,6 @@ public class Lesson4 {
         }
         return maxStr;
     }
-
 
     private static int verticalLine (char symb){
         int maxStr = 0;
@@ -188,7 +168,7 @@ public class Lesson4 {
         for (int i = 0; i < map.length; i++) {
             for (int j = 0; j < map[i].length; j++) {
                 int str = 1;
-                while (j != 0 && i < map.length - 1 && map[i][j] == symb && map[i + 1][j - 1] == symb) {
+                while (j > 0 && i < map.length - 1 && map[i][j] == symb && map[i + 1][j - 1] == symb) {
                     i++;
                     j--;
                     str++;
@@ -202,12 +182,8 @@ public class Lesson4 {
     }
 
     private static boolean checkWin(char symb, int line){
-/*В итоге вышел такой простой метод, резюмирующий все выше изложенные методы.
-Чувствую, что здесь как-то более красиво можно написать, но IntelliJ на меня ругаться начала,
-поэтому оставил пока так*/
         return horizontalLine(symb) >= line || verticalLine(symb) >= line
                || diagonalLineA(symb) >= line || diagonalLineB(symb) >= line;
-
 
     }
 }
