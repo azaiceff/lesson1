5789430287e0f6c612e909025186a7053edfa1a6
Test <zaiseffz@gmail.com>
2022-04-10 16:18:38 +0300

    Homework 5

---
+++ src/Lesson5/Employee.java
@@ -0,0 +1,28 @@
+package Lesson5;
+
+class Employee {
+    private final String fullName;
+    String post;
+    String email;
+    String tel;
+    int salary;
+    public int age;
+
+    Employee(String fullName, String post, String email, String tel, int salary, int age){
+        this.fullName = fullName;
+        this.post = post;
+        this.email = email;
+        this.tel = tel;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    String getFullName() {
+        return fullName;
+    }
+
+    void printInfo(){
+        System.out.println(fullName + " " + post + " Зарплата: " + salary + "р. Возраст: " + age);
+    }
+
+}
---
+++ src/Lesson5/Lesson5.java
@@ -0,0 +1,24 @@
+package Lesson5;
+
+public class Lesson5 {
+    public static void main(String[] args) {
+
+        Employee[] employee = {
+                new Employee("Ivanov Ivan", "Engineer", "ivivan@mailbox.com", "892312312", 30000, 41),
+                new Employee("Petrov Igor", "Тракторист", "Petr@mailbox.com", "892332548", 25000, 25),
+                new Employee("Sidorova Elena", "Доярка", "sido@mailbox.com", "892325482", 15000, 18),
+                new Employee("Baranov Alex", "Пастух", "barAlex@mailbox.com", "892354821", 28000, 53),
+                new Employee("Putin Vladimir", "President", "http://www.letters.kremlin.ru/letters/send", "8 495 606 36 02", 0, 69)
+        };
+
+        for(Employee ages : employee){
+            if (ages.age > 40){
+                if (ages.getFullName().equals("Putin Vladimir")){
+                    System.out.println("Президента не трогаем!!!"
+                            + "\nПо всем вопросам обращайтесь по тел: " + ages.tel
+                            + "\nИли напишите письмо по адресу:\n" + ages.email);
+                }else ages.printInfo();
+            }
+        }
+    }
+}
