ac73c24f92d723cbc1ebdb3f73db2cd8640aac7d
Test <zaiseffz@gmail.com>
2022-04-14 16:03:18 +0300

    Homework7 another way

--- src/Lesson7/Plate.java
+++ src/Lesson7/Plate.java
@@ -16,18 +16,11 @@ class Plate {
 /*1. Сделать так, чтобы в тарелке с едой не могло получиться отрицательного количества еды
 (например, в миске 10 еды, а кот пытается покушать 15-20).
 В методе decreaseFood как раз реализовано это условие*/
-    int decreaseFood(int appetite) {
-        if(food == 0){
-            System.out.print("Еда в тарелке закончилась, добавим в нее еще ");
-            newFood();
-        }
-        if(food - appetite > 0){
+    void decreaseFood(int appetite) {
+        if(food > appetite){
             food -= appetite;
-            return food; //здесь не имеет значение что возвращать, главное что бы число было не отрицательно
         }else{
-            int app = food - appetite;
             food = 0;
-            return app;
         }
     }
 /*5. Добавить в тарелку метод, с помощью которого можно было бы добавлять еду в тарелку.
@@ -35,7 +28,7 @@ class Plate {
     void newFood(){
         Random random = new Random();
         int newFood = random.nextInt(20) + 20;
-        System.out.println(newFood + "кг вискоса");
+        System.out.println("Еда в тарелке закончилась, добавим в нее еще " + newFood + "кг вискоса");
         food += newFood;
     }
 
